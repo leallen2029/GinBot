9f135a014fc6fa79e7c09f92f9604227dbd16b75
leallen2029 <leallen@smith.edu>
2025-11-20 15:33:59 -0500

    Update Interaction.py

--- Interaction.py
+++ Interaction.py
@@ -1,41 +1,55 @@
 def show_hand(label, hand):
-    '''prints something like: Your hand: [0:A♠] [1:7♥] [2:7♣]'''
-    text = label + ""
-    idx = 0
-    while idx < len(hand):
-        new_idx = hand[new_idx] 
-        text = text, str(idx), ":", card
-        idx = idx + 1
-    print(text)
-        
-    print("Your hand: [",def deal_hand(deck,a), "]")
+    '''prints cards and their index number
+    EX Your hand: [0:A♠] [1:7♥] [2:7♣]'''
+    
+    print(label)
+    
+    i = 0
+    while i < len(hand):
+        card = hand[i] 
+        print(str(i) + ":" + card)
+        i = i + 1
+    print("\n")
+   
+  
 
 def show_top_of_discard(discard):
-    '''prints the top card or says (empty)'''
-    if card in discard:
-        print(discard(range[0]))
+    '''prints the top card or says empty'''
+    if len(discard) > 0:
+        print("Top of discard:", discard[-1])
     else:
         print("empty")
-        
+
+  
+  
 def ask_choice(prompt, options):
     '''shows options Draw deck, Take discard, and return the chosen number'''
+    while True:
+        print(prompt)
+        
+        for key in options:
+            print(key, ":", options[key])
+            
+        my = input(">")
+        
+        if my in options:
+            return(my) #there is a chance this will not print my correctly
+        
+        print("Invalid.\n")
+
+
 
 def ask_index(prompt, limit):
     '''asks for a number and return it this tells us which card to discard'''
-
-
-def main():
-    '''starts the program: shows a simple menu (Play / Instructions / High Scores / Quit).'''
-
-
-def main_menu():
-    '''prints the menu, reads the choice with input(), calls the right function.'''
-
-
-def load_highscores()
-def save_highscores(rows):
-    '''read/write a list of past results'''
-
-
-def record_result(player_name, winner, player_pts, ai_pts):
-    '''update player’s wins/losses and append a row to highscores'''
+    while True:
+        print(prompt)
+        print("You can only pick a number between 0 and", limit - 1)
+        i = input(">")
+         
+    '''makes sure input is a digit'''
+        if i.isdigit():
+            num = int(i)
+            if 0 <= num < limit:
+                return num
+        else:    
+            print("Invalid index.")
